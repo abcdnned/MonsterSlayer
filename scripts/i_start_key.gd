852fe007c2f8ffbaae18ff0e4823ed0ee3d1ec0d
Tom Yang <tom.yang01@sap.com>
2024-07-14 00:01:12 +0800

    editing flying key

---
+++ scripts/i_start_key.gd
@@ -0,0 +1,11 @@
+extends Lootable
+
+const FLYING_KEY = preload("res://scenes/flying_key.tscn")
+
+func shoot():
+	var flying_key = FLYING_KEY.instantiate()
+	flying_key.global_position = get_parent().global_position
+	flying_key.global_rotation = get_parent().global_rotation
+	get_tree().current_scene.add_child(flying_key)
+	queue_free()
+	Tool.play_sound_2d(get_tree().current_scene, "res://sounds/dagger_woosh.mp3", flying_key.global_position)
