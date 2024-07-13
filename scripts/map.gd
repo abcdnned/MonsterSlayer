acde3229573f31b85cfe9f102e247de0fea1121b
Tom Yang <tom.yang01@sap.com>
2024-07-13 22:45:35 +0800

    world map logic move to map

--- scripts/map.gd
+++ scripts/map.gd
@@ -3,7 +3,7 @@ extends Control
 @onready var grid_container = $GridContainer
 
 var map = []
-var map_cord = []
+var cord = []
 var tile_to_cord = {}
 const START = Vector2(4, 4)
 const MAP_H = 20
@@ -84,8 +84,57 @@ func _on_player_map_pos_change(x, y):
 		player.queue_free()
 	player = KNIGHT_POTRIAT.instantiate()
 	var p = owner.tile_map.local_to_map(owner.tile_map.to_local(Vector2(x, y)))
-	for t in map_cord:
+	for t in cord:
 		if t["top_left"].x <= p.x and t["top_left"].y <= p.y and t["bottom_right"].x >= p.x and t["bottom_right"].y >= p.y:
 			tiles[t["map"].x][t["map"].y].add_child(player)
 			break
 
+
+func get_spawn_position(top_left, bottom_right):
+	var x = randf_range(top_left.x, bottom_right.x)
+	var y = randf_range(bottom_right.y, top_left.y)
+	while not can_spawn(x, y):
+		x = randf_range(top_left.x, bottom_right.x)
+		y = randf_range(bottom_right.y, top_left.y)
+	return Vector2(x, y)
+
+func can_spawn(x, y):
+	var p = owner.tile_map.local_to_map(owner.tile_map.to_local(Vector2(x, y)))
+	return owner.tile_map.get_cell_atlas_coords(0, p) != Vector2i(0, 2)
+
+func create_boime(x1, x2, y1, y2, mx, my, from, route):
+	for x in range(x1, x2 + 1):
+		for y in range(y1, y2 + 1):
+			var chance = randi_range(1, 100 + 10 + 5)
+			tile_to_cord[Vector2(x, y)] = Vector2(mx, my)
+			if chance <= 10:
+				owner.tile_map.set_cell(0, Vector2i(x, y), 0, Vector2i(0, 0), 0)
+			elif chance <= 15:
+				owner.tile_map.set_cell(0, Vector2i(x, y), 0, Vector2i(1, 0), 0)
+			else:
+				owner.tile_map.set_cell(0, Vector2i(x, y), 0, Vector2i(0, 1), 0)
+	route[Vector2(mx, my)] = null
+	var size = x2 - x1 + 1
+	cord.append({"map": Vector2(mx, my), "top_left": Vector2(x1, y1), "bottom_right": Vector2(x2, y2)})
+	# generate plain
+	if from != 3 and map[mx + 1][my] == 1 and not route.has(Vector2(mx + 1, my)):
+		create_boime(x1, x2, y1 + size, y2 + size, mx + 1, my, 1, route)
+	if from != 2 and map[mx][my + 1] == 1 and not route.has(Vector2(mx, my + 1)):
+		create_boime(x1 + size, x2 + size, y1, y2, mx, my + 1, 4, route)
+	if from != 1 and map[mx - 1][my] == 1 and not route.has(Vector2(mx - 1, my)):
+		create_boime(x1, x2, y1 - size, y2 - size, mx - 1, my, 3, route)
+	if from != 4 and map[mx][my - 1] == 1 and not route.has(Vector2(mx, my - 1)):
+		create_boime(x1 - size, x2 - size, y1, y2, mx, my - 1, 2, route)
+	# generate wall
+	if from != 3 and map[mx + 1][my] == 0:
+		for x in range(x1, x2 + 1):
+			owner.tile_map.set_cell(0, Vector2i(x, y2), 0, Vector2i(0, 2), 0)
+	if from != 2 and map[mx][my + 1] == 0:
+		for y in range(y1, y2 + 1):
+			owner.tile_map.set_cell(0, Vector2i(x2, y), 0, Vector2i(0, 2), 0)
+	if from != 1 and map[mx - 1][my] == 0:
+		for x in range(x1, x2 + 1):
+			owner.tile_map.set_cell(0, Vector2i(x, y1), 0, Vector2i(0, 2), 0)
+	if from != 4 and map[mx][my - 1] == 0:
+		for y in range(y1, y2 + 1):
+			owner.tile_map.set_cell(0, Vector2i(x1, y), 0, Vector2i(0, 2), 0)
--- scripts/world.gd
+++ scripts/world.gd
@@ -1,5 +1,6 @@
 # 5.0 target
 # TODO key and gate
+# TODO world map related code move to map.gd
 
 # Backlogs
 # Sheild charge
@@ -17,7 +18,6 @@
 # key merchants boss
 # goblin summoner
 # treasure goblin sneaker
-# world map related code move to map.gd
 # white eye attack, purple eye attack
 # lootable precise collision
 # just pressed attack
@@ -52,49 +52,12 @@ func _ready():
 	randomize()
 	load_player(PLAYER_HAMMER, Vector2(0, 0))
 	var route := {}
-	_create_boime(-10, 10, -10, 10, map.map, 4, 4, 0, map.map_cord, route)
+	map.create_boime(-10, 10, -10, 10, 4, 4, 0, route)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
 	
-func _create_boime(x1, x2, y1, y2, map, mx, my, from, cord, route):
-	for x in range(x1, x2 + 1):
-		for y in range(y1, y2 + 1):
-			var chance = randi_range(1, 100 + 10 + 5)
-			self.map.tile_to_cord[Vector2(x, y)] = Vector2(mx, my)
-			if chance <= 10:
-				tile_map.set_cell(0, Vector2i(x, y), 0, Vector2i(0, 0), 0)
-			elif chance <= 15:
-				tile_map.set_cell(0, Vector2i(x, y), 0, Vector2i(1, 0), 0)
-			else:
-				tile_map.set_cell(0, Vector2i(x, y), 0, Vector2i(0, 1), 0)
-	route[Vector2(mx, my)] = null
-	var size = x2 - x1 + 1
-	cord.append({"map": Vector2(mx, my), "top_left": Vector2(x1, y1), "bottom_right": Vector2(x2, y2)})
-	# generate plain
-	if from != 3 and map[mx + 1][my] == 1 and not route.has(Vector2(mx + 1, my)):
-		_create_boime(x1, x2, y1 + size, y2 + size, map, mx + 1, my, 1, cord, route)
-	if from != 2 and map[mx][my + 1] == 1 and not route.has(Vector2(mx, my + 1)):
-		_create_boime(x1 + size, x2 + size, y1, y2, map, mx, my + 1, 4, cord, route)
-	if from != 1 and map[mx - 1][my] == 1 and not route.has(Vector2(mx - 1, my)):
-		_create_boime(x1, x2, y1 - size, y2 - size, map, mx - 1, my, 3, cord, route)
-	if from != 4 and map[mx][my - 1] == 1 and not route.has(Vector2(mx, my - 1)):
-		_create_boime(x1 - size, x2 - size, y1, y2, map, mx, my - 1, 2, cord, route)
-	# generate wall
-	if from != 3 and map[mx + 1][my] == 0:
-		for x in range(x1, x2 + 1):
-			tile_map.set_cell(0, Vector2i(x, y2), 0, Vector2i(0, 2), 0)
-	if from != 2 and map[mx][my + 1] == 0:
-		for y in range(y1, y2 + 1):
-			tile_map.set_cell(0, Vector2i(x2, y), 0, Vector2i(0, 2), 0)
-	if from != 1 and map[mx - 1][my] == 0:
-		for x in range(x1, x2 + 1):
-			tile_map.set_cell(0, Vector2i(x, y1), 0, Vector2i(0, 2), 0)
-	if from != 4 and map[mx][my - 1] == 0:
-		for y in range(y1, y2 + 1):
-			tile_map.set_cell(0, Vector2i(x1, y), 0, Vector2i(0, 2), 0)
-
 func _on_player_hero_death():
 	lose_scene.visible = true
 
@@ -149,21 +112,10 @@ func get_money(m):
 	coins.text = "Coins: " + str(money)
 
 func spawn(type, top_left, bottom_right):
-	var spawn_position = get_spawn_position(top_left, bottom_right)
+	var spawn_position = map.get_spawn_position(top_left, bottom_right)
 	var thing = type.instantiate()
 	thing.position = spawn_position
 	add_child(thing)
 	return thing
 
-func get_spawn_position(top_left, bottom_right):
-	var x = randf_range(top_left.x, bottom_right.x)
-	var y = randf_range(bottom_right.y, top_left.y)
-	while not can_spawn(x, y):
-		x = randf_range(top_left.x, bottom_right.x)
-		y = randf_range(bottom_right.y, top_left.y)
-	return Vector2(x, y)
-
-func can_spawn(x, y):
-	var p = tile_map.local_to_map(tile_map.to_local(Vector2(x, y)))
-	return tile_map.get_cell_atlas_coords(0, p) != Vector2i(0, 2)
 	
